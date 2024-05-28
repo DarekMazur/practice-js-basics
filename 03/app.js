6b79e5424cb2e3bb67a239bd23e54f973ea62917
Darek Mazur <dev@darekmazur.it>
2024-05-28 18:03:25 +0200

    Create getSum method

--- 03/app.js
+++ 03/app.js
@@ -7,12 +7,37 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
+const getSum = (a, b, c) => {
+  try {
+    if (isNaN(Number(a)) || isNaN(b) || isNaN(c)) {
+      throw new Error('One of value is not a number');
+    }
+		
+		const inputs = []
+		
+		inputs.push(Math.round(parseFloat(a)))
+		inputs.push(Math.round(parseFloat(b)))
+		inputs.push(Math.round(parseFloat(c)))
+		
+		const sortedInput = inputs.sort((a, b) => {
+			if (a < b) {
+				return 1;
+			}
+			if (a > b) {
+				return -1;
+			}
+			return 0;
+		})
+		
+		return sortedInput[0] + sortedInput[1]
+		
+  } catch (error) {
+    console.error(error.message);
+  }
+}
 
-
-
-
-
+console.log(getSum(a,b,c))
 
 function randomNumber(min, max) {
-    return Math.round((Math.random() * (max - min)) + min);
+  return Math.round((Math.random() * (max - min)) + min);
 }
